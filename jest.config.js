094e3c37b9610d38d5d88a4c111fc52dfd537fcb
Roy Chiu <roychiu@roblox.com>
2023-11-17 17:29:03 -0800

    update jest module

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
+  modulePaths: ['<rootDir>'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
